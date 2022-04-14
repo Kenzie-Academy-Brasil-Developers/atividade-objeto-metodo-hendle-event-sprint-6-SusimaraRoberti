3f184739d6ba5ccd24cb5f28abf0ec3e4e08e862
SusimaraRoberti <susimararoberti@outlook.com>
2022-04-14 12:57:57 -0300

    implementado

--- click_to_advance.js
+++ click_to_advance.js
@@ -1,40 +1,56 @@
 const flower_images = ["flower0.png", "flower1.png", "flower2.png"];
 const container_div = document.getElementById("container_div");
 
-/* frame_images is an array of image URLs.
- * target_div is the HTML element within which the image should be appended.
+/* frame_images is an array of image URLs. 
+Frame_imagens é um array das imagens
+ * target_div is the HTML element within which the image should be appended. 
+target_div é o elemento html onde as imagens serão "append"
  * x and y are the coordinates for where to position the image.
+X e Y são as coordenadas de onde posicionar a imagem
  */
 class ClickToAdvance {
-    constructor (frame_images, target_div, x, y) {
-        this.images = frame_images; // An array of image urls
-        this.frame = 0;             // Index of the frame to show
-        this.img = document.createElement("img");
-        this.img.src = this.images[0];
-        this.img.style.position = "absolute";
-        this.img.style.left = x + "px";
-        this.img.style.top = y + "px";
-        target_div.appendChild(this.img);
-    }
-    /* TODO: Implement a prototype method named handleEvent (see Note 1 below).
+  constructor(frame_images, target_div, x, y) {
+    this.images = frame_images; // An array of image urls
+    this.frame = 0; // Index of the frame to show
+    this.img = document.createElement("img");
+    this.img.src = this.images[0];
+    this.img.style.position = "absolute";
+    this.img.style.left = x + "px";
+    this.img.style.top = y + "px";
+    target_div.appendChild(this.img);
+    this.img.addEventListener("click", this);
+  }
+  /* TODO: Implement a prototype method named handleEvent (see Note 1 below).
      * "handleEvent" should advance to the next frame in the sequence.
+    Implemente um método de protótipo chamado handleEvent (veja a Nota 1 abaixo). 
+     "handleEvent" deve avançar para o próximo quadro na sequência.
      * 
-     * In other words, add 1 to this.frame, and set
-     * this.img.src = this.images[this.frame]
+     * In other words, add 1 to this.frame, and set this.img.src = this.images[this.frame]
+     * Em outras palavras, adicione 1 a this.frame e defina this.img.src = this.images[this.frame]
      * 
      * Dont increase this.frame beyond the number of frame_images available.
+     * Não aumente this.frame além do número de frame_images disponíveis.
      * 
      * Once you have implemented the handleEvent method, you can uncomment the
      * line below to add this object as the click event listener for the image.
+     * Depois de implementar o método handleEvent, você pode descomentar o
+     * abaixo para adicionar este objeto como o ouvinte do evento de clique para a imagem.
      */
-     
-     // this.img.addEventListener("click", this);            // (see Note 1 below)
+  handleEvent() {
+    this.frame += 1;
+    if (this.frame > 2) {
+      this.frame = 0;
+    }
+    this.img.src = this.images[this.frame];
+  }
+  //   this.img.addEventListener("click", this);
+  // (see Note 1 below)
 }
 
 for (let i = 0; i < 5; i++) {
-    const x = 150 * i;
-    const y = Math.floor(Math.random() * 500);
-    const flower = new ClickToAdvance(flower_images, container_div, x, y);
+  const x = 150 * i;
+  const y = Math.floor(Math.random() * 500);
+  const flower = new ClickToAdvance(flower_images, container_div, x, y);
 }
 
 /* NOTE 1 -------
@@ -42,5 +58,6 @@ for (let i = 0; i < 5; i++) {
 "The event listener can be specified as... an object... whose handleEvent() method serves as the callback function." <https://mdn.io/addEventListener#The_event_listener_callback>.
 
 In our case, this object is the instance of our ClickToAdvance class, and as usual, we're representing it with `this`. 
+No nosso caso, este objeto é a instância da nossa classe ClickToAdvance e, como sempre, estamos representando com `this`.
 
 You could pass a callback function directly to addEventListener, but in this case, we are making use of a different addEventListener feature: if we pass it a normal object (like `this`), instead of a function, it will try to look on that object for a method called "handleEvent". If it finds that method name on the object, it will then execute that it as though you had passed it directly as the callback function argument. One benefit of this approach is that you don't need to bind the callback function to the instance or worry about the differences between arrow functions and standard functions. */
